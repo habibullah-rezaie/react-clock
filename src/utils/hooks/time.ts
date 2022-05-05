cbc6038a90ff3d79659da7c03656d7a7c0d66196
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-05 18:41:06 +0430

    Create useTimer hook

--- src/utils/hooks/time.ts
+++ src/utils/hooks/time.ts
@@ -2,20 +2,39 @@ import * as React from "react";
 
 export type TimerState = "inactive" | "paused" | "running" | "ended";
 
-export function useTimer(startTime?: number) {
-  const [timerState, setTimerState] = React.useState<TimerState>("inactive");
+export function useTimer(
+  startTime?: number,
+  initialState: TimerState = "inactive"
+) {
+  const [timerState, setTimerState] = React.useState<TimerState>(initialState);
   const [seconds, setSeconds] = React.useState(startTime || 0);
 
   React.useEffect(() => {
-    const timerInterval = window.setInterval(() => {
-      if (seconds === 0 && timerState === "running") {
-        setTimerState("ended");
-      }
-      setSeconds((sec) => sec - 1);
-    }, 1000);
+    let timerInterval: null | number = null;
 
-    return () => window.clearInterval(timerInterval);
+    if (timerState === "ended") {
+      setTimerState("inactive");
+    }
+
+    if (timerState === "running" && seconds !== 0) {
+      timerInterval = window.setInterval(() => {
+        if (seconds === 1) {
+          setTimerState("ended");
+          setSeconds(0)
+        } else {
+          setSeconds((sec) => sec - 1);
+        }
+      }, 1000);
+    } else if (seconds === 0) {
+      setTimerState("inactive");
+    }
+
+    return () => {
+      if (timerInterval != null) {
+        window.clearInterval(timerInterval);
+      }
+    };
   }, [seconds, timerState]);
 
-  return { timerState, setTimerState, seconds };
+  return { timerState, setTimerState, seconds, setSeconds };
 }
